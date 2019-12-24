9911f0d4fa62e2ed888ba5b7bb8b5bf716b51c0f
Phillip Berg <phillipb1991@msn.com>
2019-12-24 09:42:29 -0600

    set up tests

---
+++ In Array/solution.js
@@ -0,0 +1,10 @@
+// TESTS
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+a1 = ["xyz", "live", "strong"]
+Test.assertSimilar(inArray(a1, a2), ["live", "strong"])
+a1 = ["live", "strong", "arp"]
+Test.assertSimilar(inArray(a1, a2), ["arp", "live", "strong"])
+a1 = ["tarp", "mice", "bull"]
+Test.assertSimilar(inArray(a1, a2), [])
