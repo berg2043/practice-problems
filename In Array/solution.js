75505b2fbadd2d6a605ee050c4d539047f2d7130
Phillip Berg <phillipb1991@msn.com>
2019-12-24 09:51:16 -0600

    solved problem

--- In Array/solution.js
+++ In Array/solution.js
@@ -1,10 +1,23 @@
+function inArray(array1, array2){
+  let holder = [];
+  for(str of array1){
+    for(str2 of array2)
+      if(str2.includes(str)){
+        holder.push(str);
+        break;
+    }
+  }
+  holder.sort();
+  return holder;
+}
+
 // TESTS
 
 a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
 
 a1 = ["xyz", "live", "strong"]
-Test.assertSimilar(inArray(a1, a2), ["live", "strong"])
+console.log(inArray(a1, a2), ["live", "strong"])
 a1 = ["live", "strong", "arp"]
-Test.assertSimilar(inArray(a1, a2), ["arp", "live", "strong"])
+console.log(inArray(a1, a2), ["arp", "live", "strong"])
 a1 = ["tarp", "mice", "bull"]
-Test.assertSimilar(inArray(a1, a2), [])
+console.log(inArray(a1, a2), [])
