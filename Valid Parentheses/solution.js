392b45c995f4a5dae818170503c0e21d77475a75
Phillip Berg <phillipb1991@msn.com>
2019-12-26 09:54:52 -0600

    started solution and added test

--- Valid Parentheses/solution.js
+++ Valid Parentheses/solution.js
@@ -1,5 +1,12 @@
-
+function validParentheses(parens){
+  const arr = parens.split('');
+  if(arr.indexOf(')')<arr.indexOf('(')){
+    return false;
+  }
+  return true;
+}
 
 // TESTS
 console.log(validParentheses( "()" ), true);
 console.log(validParentheses( "())" ), false);
+console.log(validParentheses( ")()(" ), false);
