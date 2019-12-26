1ef3d9239f1cccf9b7fcbb5aa78e9a2224df89fa
Phillip Berg <phillipb1991@msn.com>
2019-12-25 23:22:57 -0600

    Solved

--- Pyramid/solution.js
+++ Pyramid/solution.js
@@ -1,6 +1,20 @@
+function findNb(m){
+  let remaining = m;
+  let base = 0;
+  while(remaining>0){
+    base += 1;
+    remaining -= Math.pow(base, 3);
+  }
+  if(remaining < 0){
+    return -1;
+  } else {
+    return base;
+  }
+}
 
 
 // Tests
+console.log(findNb(27))
 console.log(findNb(1071225), 45)
 console.log(findNb(91716553919377),  -1)
 console.log(findNb(135440716410000), 4824)
