b628bd1fe2c00ab4abc79fd2a65aa8d511b16594
Phillip Berg <phillipb1991@msn.com>
2019-12-26 10:16:52 -0600

    solved

--- Valid Parentheses/solution.js
+++ Valid Parentheses/solution.js
@@ -1,12 +1,23 @@
+// Not a fan of turning a into a string to break the addition, but it works
 function validParentheses(parens){
   const arr = parens.split('');
-  if(arr.indexOf(')')<arr.indexOf('(')){
-    return false;
-  }
-  return true;
+  return (arr.reduce((a,c)=>{
+    if(c==='('){
+      a++;
+      return a;
+    } else if (c===')'){
+      a--;
+      if(a<0){
+        a = '';
+      } else {
+        return a;
+      }
+    }
+  },0) === 0)
 }
 
 // TESTS
 console.log(validParentheses( "()" ), true);
 console.log(validParentheses( "())" ), false);
 console.log(validParentheses( ")()(" ), false);
+console.log(validParentheses( ")()()()(" ), false);
