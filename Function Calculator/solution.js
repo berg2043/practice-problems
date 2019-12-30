dc4783b1b6a82395fc9e8b68bdc5cea2ccbcc9db
Phillip Berg <phillipb1991@msn.com>
2019-12-30 11:53:23 -0600

    Solved

--- Function Calculator/solution.js
+++ Function Calculator/solution.js
@@ -1,25 +1,119 @@
 // Numbers
-function zero(){}
-function one(){}
-function two(){}
-function three(){}
-function four(){}
-function five(){}
-function six(){}
-function seven(){}
-function eight(){}
-function nine(){}
+function zero(incoming){
+  if(incoming){
+    incoming.push(0);
+    return operationIdentifier(incoming);
+  } else {
+    return 0;
+  }
+}
+function one(incoming){
+  if(incoming){
+    incoming.push(1);
+    return operationIdentifier(incoming);
+  } else {
+    return 1;
+  }
+}
+function two(incoming){
+  if(incoming){
+    incoming.push(2);
+    return operationIdentifier(incoming);
+  } else {
+    return 2;
+  }
+}
+function three(incoming){
+  if(incoming){
+    incoming.push(3);
+    return operationIdentifier(incoming);
+  } else {
+    return 3;
+  }
+}
+function four(incoming){
+  if(incoming){
+    incoming.push(4);
+    return operationIdentifier(incoming);
+  } else {
+    return 4;
+  }
+}
+function five(incoming){
+  if(incoming){
+    incoming.push(5);
+    return operationIdentifier(incoming);
+  } else {
+    return 5;
+  }
+}
+function six(incoming){
+  if(incoming){
+    incoming.push(6);
+    return operationIdentifier(incoming);
+  } else {
+    return 6;
+  }
+}
+function seven(incoming){
+  if(incoming){
+    incoming.push(7);
+    return operationIdentifier(incoming);
+  } else {
+    return 7;
+  }
+}
+function eight(incoming){
+  if(incoming){
+    incoming.push(8);
+    return operationIdentifier(incoming);
+  } else {
+    return 8;
+  }
+}
+function nine(incoming){
+  if(incoming){
+    incoming.push(9);
+    return operationIdentifier(incoming);
+  } else {
+    return 9;
+  }
+}
+
+// Math function to make code drier
+function operationIdentifier(arr){
+  switch (arr[1]) {
+    case '+':
+      return arr[2] + arr[0];
+    case '-':
+      return arr[2] - arr[0];
+    case '*':
+      return arr[2] * arr[0];
+    case '/':
+      return Math.floor(arr[2] / arr[0]);
+    default:
+      return 'inpropper middle function';
+  }
+}
 
 // Operations
-function plus(){}
-function minus(){}
-function times(){}
-function dividedBy(){}
+function plus(num){
+  return [num, '+'];
+}
+function minus(num){
+  return [num, '-'];
+}
+function times(num){
+  return [num, '*'];
+}
+function dividedBy(num){
+  return [num, '/'];
+}
 
 // Tests
 
 console.log(seven(times(five())), 35)
-console.log(four(plus(nine())), 13)
+console.log(four(plus(nine())), 13) // FOCUS
 console.log(eight(minus(three())), 5)
 console.log(six(dividedBy(two())), 3)
 console.log(seven(times(five())), 35);
