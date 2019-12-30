febbc057e99ac7fd32bac39ccbcc2487c74ea153
Phillip Berg <phillipb1991@msn.com>
2019-12-30 11:36:17 -0600

    fixed typo

--- Function Calculator/solution.js
+++ Function Calculator/solution.js
@@ -1,27 +1,27 @@
 // Numbers
-function zero() {}
-function one() {}
-function two() {}
-function three() {}
-function four() {}
-function five() {}
-function six() {}
-function seven() {}
-function eight() {}
-function nine() {}
+function zero(){}
+function one(){}
+function two(){}
+function three(){}
+function four(){}
+function five(){}
+function six(){}
+function seven(){}
+function eight(){}
+function nine(){}
 
 // Operations
-function plus() {}
-function minus() {}
-function times() {}
-function dividedBy() {}
+function plus(){}
+function minus(){}
+function times(){}
+function dividedBy(){}
 
 // Tests
 
-cosnole.log(seven(times(five())), 35)
-cosnole.log(four(plus(nine())), 13)
-cosnole.log(eight(minus(three())), 5)
-cosnole.log(six(dividedBy(two())), 3)
+console.log(seven(times(five())), 35)
+console.log(four(plus(nine())), 13)
+console.log(eight(minus(three())), 5)
+console.log(six(dividedBy(two())), 3)
 console.log(seven(times(five())), 35);
 console.log(four(plus(nine())), 13);
 console.log(eight(minus(three())), 5);
