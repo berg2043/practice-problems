529833b0e274890b49b6df2960e89ccc5427ccf4
Phillip Berg <phillipb1991@msn.com>
2019-12-30 11:33:50 -0600

    added tests

---
+++ Function Calculator/solution.js
@@ -0,0 +1,12 @@
+
+
+// Tests
+
+cosnole.log(seven(times(five())), 35)
+cosnole.log(four(plus(nine())), 13)
+cosnole.log(eight(minus(three())), 5)
+cosnole.log(six(dividedBy(two())), 3)
+console.log(seven(times(five())), 35);
+console.log(four(plus(nine())), 13);
+console.log(eight(minus(three())), 5);
+console.log(six(dividedBy(two())), 3);
