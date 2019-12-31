f5b45954641c53b65ee3b5ab340b4f1804904c49
Phillip Berg <phillipb1991@msn.com>
2019-12-31 10:30:35 -0600

    added more tests and solved

--- Cash Register/solution.js
+++ Cash Register/solution.js
@@ -1,4 +1,29 @@
-
+function tickets(peopleInLine){
+  const register = {
+    '25': 0,
+    '50': 0,
+    '100': 0
+  }
+  for(person of peopleInLine){
+    register[person]+=1
+    if(50 === person){
+      register['25']-=1
+      if(register['25'] < 0){
+        return 'NO';
+      }
+    } else if (100 === person){
+      if(register['50'] >= 1 && register['25'] >= 1){
+        register['50']-=1;
+        register['25']-=1;
+      } else if (register['25']>=3){
+        register['25']-=3;
+      } else {
+        return 'NO';
+      }
+    }
+  }
+  return 'YES';
+}
 
 
 // Tests
@@ -7,3 +32,6 @@ console.log(tickets([25, 100]), "NO");
 console.log(tickets([25, 25, 50, 50, 100]), "NO");
 console.log(tickets([25, 25, 50, 50]), "YES");
 console.log(tickets([25, 100]), "NO");
+console.log(tickets([ 25, 25, 25, 25, 25, 100, 100 ]), "NO");
+console.log(tickets([ 25, 25, 25, 100, 25, 25, 25, 100, 25, 50, 25, 100, 25, 100, 25 ]), "NO");
+console.log(tickets([ 25, 50, 25, 100, 25, 50, 25, 100, 25, 25, 50, 100, 25, 25, 25, 100, 50, 25 ]), "NO");
