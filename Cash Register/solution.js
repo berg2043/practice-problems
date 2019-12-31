dfd0030f8233fb3d92810459900aeec1ec12c103
Phillip Berg <phillipb1991@msn.com>
2019-12-31 10:13:45 -0600

    added tests

---
+++ Cash Register/solution.js
@@ -0,0 +1,9 @@
+
+
+
+// Tests
+console.log(tickets([25, 25, 50]), "YES");
+console.log(tickets([25, 100]), "NO");
+console.log(tickets([25, 25, 50, 50, 100]), "NO");
+console.log(tickets([25, 25, 50, 50]), "YES");
+console.log(tickets([25, 100]), "NO");
