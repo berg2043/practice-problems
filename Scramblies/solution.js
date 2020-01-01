4b837050316e8994c38760f763e9efea3f41001f
Phillip Berg <phillipb1991@msn.com>
2020-01-01 10:05:46 -0600

    added tests

---
+++ Scramblies/solution.js
@@ -0,0 +1,11 @@
+
+// Tests
+
+console.log(scramble('rkqodlw', 'world'), True);
+console.log(scramble('cedewaraaossoqqyt', 'codewars'), True);
+console.log(scramble('katas', 'steak'), False);
+console.log(scramble('scriptjava','javascript'),true);
+console.log(scramble('scriptingjava','javascript'),true);
+console.log(scramble('scriptsjava','javascripts'),true);
+console.log(scramble('jscripts','javascript'),false);
+console.log(scramble('aabbcamaomsccdd','commas'),true);
