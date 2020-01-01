7277d975f69d98d6df52552d50dcfe212c61d3d3
Phillip Berg <phillipb1991@msn.com>
2020-01-01 10:27:44 -0600

    Inefficient solution

--- Scramblies/solution.js
+++ Scramblies/solution.js
@@ -1,9 +1,20 @@
+function scramble(str1, str2) {
+  const arr = str1.split('');
+  for(char of str2){
+    if(arr.indexOf(char)>=0){
+      arr.splice(arr.indexOf(char),1);
+    } else{
+      return false;
+    }
+  }
+  return true;
+}
 
 // Tests
 
-console.log(scramble('rkqodlw', 'world'), True);
-console.log(scramble('cedewaraaossoqqyt', 'codewars'), True);
-console.log(scramble('katas', 'steak'), False);
+console.log(scramble('rkqodlw', 'world'), true);
+console.log(scramble('cedewaraaossoqqyt', 'codewars'), true);
+console.log(scramble('katas', 'steak'), false);
 console.log(scramble('scriptjava','javascript'),true);
 console.log(scramble('scriptingjava','javascript'),true);
 console.log(scramble('scriptsjava','javascripts'),true);
