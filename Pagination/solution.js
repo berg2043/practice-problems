19d6c9ba1e2e7e9aeadf820cd04f5f1c6c2886cc
Phillip Berg <phillipb1991@msn.com>
2020-01-05 10:15:16 -0600

    Solved

--- Pagination/solution.js
+++ Pagination/solution.js
@@ -1,3 +1,39 @@
+class PaginationHelper{
+  constructor (collection, itemsPerPage) {
+    this.collection = collection;
+    this.itemsPerPage = itemsPerPage
+  }
+  // returns the number of items within the entire collection
+  itemCount = () => {
+    return this.collection.length;
+  }
+  
+  // returns the number of pages
+  pageCount = ()=> {
+    return Math.ceil(this.collection.length/this.itemsPerPage);
+  }
+
+  // returns the number of items on the current page. page_index is zero based.
+  // this method should return -1 for pageIndex values that are out of range
+  pageItemCount = (pageIndex) => {
+    if((1+pageIndex)*this.itemsPerPage <= this.collection.length){
+      return this.itemsPerPage;
+    }
+    if(this.collection.length-((pageIndex)*this.itemsPerPage)>-1){
+      return this.collection.length-((pageIndex)*this.itemsPerPage);
+    }
+    return -1;
+  }
+  
+  // determines what page an item is on. Zero based indexes
+  // this method should return -1 for itemIndex values that are out of range
+  pageIndex = (itemIndex) => {
+    if(Math.floor(itemIndex/this.itemsPerPage)<this.pageCount() && Math.floor(itemIndex/this.itemsPerPage)>=0){
+      return Math.floor(itemIndex/this.itemsPerPage)
+    }
+    return -1;
+  }
+}
 
 // Tests
 
